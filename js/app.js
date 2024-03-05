7a4457a07a9366508e40ab31aaab9470cc19c467
GiorgettaR <giorgetta.roberto@gmail.com>
2024-03-05 16:39:54 +0100

    Fix: html/js - modifica inserimento sconto

--- js/app.js
+++ js/app.js
@@ -2,39 +2,38 @@
 
 // gestione button
 
-const submitElement = document.getElementById('submit') //element
+const submitElement = document.getElementById('submit') //element or null
+
+let km , discount, price;
 
 submitElement.addEventListener ('click' , function() {
     // acquisire dati inseriti dall'utente
 
-    const inputLunghezzaViaggioElement = document.getElementById('lunghezzaViaggio'); // element or null
-    // console.log(inputLunghezzaViaggioElement); 
-    const inputEtaElement = document.getElementById('eta'); // element or null
-    // console.log(inputEtaElement);
+    const inputTripLengthoElement = document.getElementById('tripLength'); // element or null
+    const inputAgeElement = document.getElementById('age'); // element or null
 
     // salvare i dati acquisiti e formattare
 
-    let km = inputLunghezzaViaggioElement.value; // string
+    km = inputTripLengthoElement.value; // string
     km = parseFloat(km); //number
-    console.log('lunghezza del viaggio: ' + km + ' km');
-    let eta = inputEtaElement.value; //string
-    eta = parseInt(eta); //number
-    console.log('età del passeggero: ' + eta + ' anni');
+    console.log('Lunghezza del viaggio: ' + km + ' km');
+
+    const inputDiscountElement = document.getElementById('discount'); // element or null
+    discount = inputDiscountElement.value; //string
 
     // calcolo costo base
 
     const pricePerKm = 0.21;
-    let price = km * pricePerKm;
-    console.log('prezzo base: ' + price + ' eur');
+    price = km * pricePerKm;
+    console.log('Prezzo base: ' + price + ' eur');
 
     // calcolo costo scontato
 
-    if (eta < 18) {
+    if (discount  == 'minorenni') {
         price = price * 0.8;
-        console.log('prezzo scontato minorenni: ' + price.toFixed(2) + ' eur')
-    }   else if (eta > 64) {
+        console.log('Prezzo scontato minorenni (-20%): ' + price.toFixed(2) + ' eur')
+    }   else if (discount == 'anziani') {
         price = price * 0.6;
-        console.log('prezzo scontato anziani: ' + price.toFixed(2) + ' eur')
+        console.log('Prezzo scontato anziani (-40%): ' + price.toFixed(2) + ' eur')
     }
-
 })
