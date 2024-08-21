4195df6a21d4e90f6b3bd5fca0883ca409db521c
GiorgettaR <giorgetta.roberto@gmail.com>
2024-08-21 19:12:51 +0200

    reset for update / SCAFFHOLDING

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header msg="Yea" />
+</template>
+
+<style lang ="scss">
+@use './style/general.scss';
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <div class="container header">
+    <div class="row">
+      <div class="col-12 head">
+        <p>{{ msg }}</p>
+      </div>
+      <div class="col-12 menù">
+        
+      </div>
+      <div class="col-12 news">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang ="scss">
+
+</style>
