5ccf7e13b9c0d46f64fe05709f262da3470f7dbd
GiorgettaR <giorgetta.roberto@gmail.com>
2024-08-21 20:15:15 +0200

    ADD: header structure, file store.js

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,57 @@
-<script setup>
+<script>
+import { store } from '../store.js';
 
-defineProps({
-  msg: String,
-})
+export default {
+
+  data() {
+    return {
+    }
+  }
+}
 
 </script>
 
 <template>
   <div class="container header">
     <div class="row">
-      <div class="col-12 head">
-        <p>{{ msg }}</p>
+      <div class="col-12 head d-flex justify-content-between">
+          <a href="#">Siti del gruppo</a>
+          <a href="#">Whistleblowing-segnalazioni</a>
+          <a href="#">TreniItalia for Business</a>
+          <a href="#">Cerca il Biglietto</a>
+          <a href="#">Assistenza</a>
+          <button class="login">
+            <img src="" alt="">
+            Area Riservata
+          </button>
+          <div class="search">
+            <img src="" alt="">
+          </div>
+          <div class="language">
+            ITA
+            <select name="" id=""></select>
+          </div>
+        </div>
+
+      <div class="col-12 menù d-flex justify-content-between">
+        <img class="logo" src="" alt="">
+        <a href="#">CHI SIAMO</a>
+        <a href="#">FRECCE</a>
+        <a href="#">INTERCITY E INTERNAZIONALI</a>
+        <a href="#">REGIONALE</a>
+        <a href="#">OFFERTE</a>
+        <a href="#">Assistenza</a>
+        <a href="#">SERVIZI</a>
+        <a href="#">CARTAFRECCIA E-GO</a>
+        <a href="#">INFO</a>
       </div>
-      <div class="col-12 menù">
-        
-      </div>
-      <div class="col-12 news">
-        
+
+      <div class="col-12 news d-flex">
+        <a href="#">
+          <img src="" alt="">
+          INFOTRAFFICO
+        </a>
+        <a class="flex-grow-1" href="">Notizie varie a rotazione</a>
       </div>
     </div>
   </div>
